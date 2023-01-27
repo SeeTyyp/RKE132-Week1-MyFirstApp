0097eea3aa44d2e61775e9936179de85978dcd75
SeeTyyp <puusepp.imre@gmail.com>
2023-01-27 18:48:52 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// rakendus küsib kasutajal sisestada tema nime
+// rakendus tevitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //output
+//string - sõne
+string userName = userName = Console.ReadLine();//input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //output
+
+// string interpolation
+Console.WriteLine($"Hello, {userName}!"); //output
